0946d4474816ca25e1c4a8bf8969c2bec4f172ca
Daniel Florez Salgado <d.florez@globant.com>
2019-03-15 18:17:56 -0500

    Cache 2

--- 04-cache-offline/sw.js
+++ 04-cache-offline/sw.js
@@ -1,22 +1,53 @@
+// const CACHE_NAME = 'cache-1';
+const CACHE_STATIC_NAME = 'static-v1';
+const CACHE_DYNAMIC_NAME = 'dynamic-v1';
+
+const CACHE_INMUTABLE_NAME = 'inmutable-v1';
+
 self.addEventListener('install', e => {
 
-    const cacheProm = caches.open('cache-1')
+    const cacheProm = caches.open(CACHE_STATIC_NAME)
         .then(cache => {
             return cache.addAll([
                 '/',
                 '/index.html',
                 '/css/style.css',
                 '/img/main.jpg',
-                'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
                 '/js/app.js'
             ]);
         });
 
-    e.waitUntil(cacheProm);
+    const cacheImutable = caches.open(CACHE_INMUTABLE_NAME)
+        .then(cache => {
+            return cache.addAll([
+                'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
+            ]);
+        });
+
+    e.waitUntil(Promise.all([cacheProm, cacheImutable]));
 });
 
 self.addEventListener('fetch', e => {
 
-    // cache only (todo desde el cache)
-    e.respondWith(caches.match(e.request));
+    // 2. Cache with network fallback
+    const respuesta = caches.match(e.request)
+        .then(res => {
+
+            if (res) return res;
+
+            return fetch(e.request)
+                .then(newResp => {
+                    caches.open(CACHE_DYNAMIC_NAME)
+                        .then(cache => {
+                            cache.put(e.request, newResp);
+                        });
+
+                    return newResp.clone();
+                });
+        });
+
+    e.respondWith(respuesta);
+
+    // 1. Cache only (todo desde el cache)
+    // e.respondWith(caches.match(e.request));
 });
