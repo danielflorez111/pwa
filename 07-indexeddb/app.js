d37d16c0befcf3969d144356e5bc50c6df18374a
Daniel Florez Salgado <d.florez@globant.com>
2019-03-21 19:14:45 -0500

    Pouch db

--- 07-indexeddb/app.js
+++ 07-indexeddb/app.js
@@ -1,4 +1,3 @@
-
 // indexedDB: Reforzamiento
 let request = window.indexedDB.open('mi-database', 1);
 
@@ -9,4 +8,38 @@ request.onupgradeneeded = event => {
     db.createObjectStore('heroes', {
         keyPath: 'id'
     });
+};
+
+// Manejor de errores
+request.onerror = event => {
+    console.log('DB error:', event.target.error);
+};
+
+// Insertar datos
+request.onsuccess = event => {
+    let db = event.target.result;
+    let heroesData = [
+        { id: '111', heroe: 'Spiderman', mensaje: 'Aquí su amigo Spiderman' },
+        { id: '222', heroe: 'Ironman', mensaje: 'Aquí en mi nuevo Mark 50' }
+    ];
+
+    let heroesTransaction = db.transaction('heroes', 'readwrite');
+
+    heroesTransaction.onerror = event => {
+        console.log('Error guardando', event.target.error);
+    };
+
+    heroesTransaction.oncomplete = event => {
+        console.log('Transaccion hecha', event);
+    };
+
+    let heroesStore = heroesTransaction.objectStore('heroes');
+
+    for (let heroe of heroesData) {
+        heroesStore.add(heroe);
+    }
+
+    heroesStore.onsuccess = event => {
+        console.log('Nuevo item agregado');
+    }
 };
